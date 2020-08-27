9eb0c0253c7753e3112c32686cf10b556b9b6487
frank@softwaretailor.com <frank@softwaretailor.com>
2020-08-27 14:34:34 +0800

    init

---
+++ Data/CategoryService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DownloadWebsite.Data
+{
+    public class CategoryService
+    {
+        private SoftwareDbContext db;
+        public CategoryService(SoftwareDbContext db)
+        {
+            this.db = db;
+        }
+        public List<Category> GetAll()
+        {
+            return db.Categories.ToList();
+        }
+        public void Add(Category obj)
+        {
+            db.Categories.Add(obj);
+            db.SaveChanges();
+        }
+        public void Update(Category inputObj)
+        {
+            var obj = db.Categories.FirstOrDefault(x => x.ID == inputObj.ID);
+            PropertyCopy.Copy(inputObj, obj);
+            db.SaveChanges();
+        }
+        public void Delete(Category animal)
+        {
+            db.Categories.Remove(db.Categories.FirstOrDefault(x => x.ID == animal.ID));
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Data/SoftwareDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DownloadWebsite.Data
+{
+    public class SoftwareDbContext: DbContext
+    {
+        public SoftwareDbContext(DbContextOptions<SoftwareDbContext> options = null) : base(options)
+        {
+        }
+        public DbSet<File> Files { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+    public class File
+    {
+        public int ID { get; set; }
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int FileSize { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+    }
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
